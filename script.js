b55c1fb01c044fcb0954f04de70e3d0ffe1f2403
Buggerdas <106815811+Buggerdas@users.noreply.github.com>
2022-11-27 18:07:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
+        
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
          byeSpeaker.speak(names[i]);
@@ -9,5 +10,6 @@
          else {
         helloSpeaker.speak(names[i]);
     }
+        console.log("i = " + i);
 }
 })();
